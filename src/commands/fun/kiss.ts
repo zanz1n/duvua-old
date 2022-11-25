b28befd9bfb289b132082f8a982d2386e44a392f
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-11-25 19:39:20 -0300

    kiss command added

--- src/commands/fun/kiss.ts
+++ src/commands/fun/kiss.ts
@@ -1,5 +1,9 @@
-import { ApplicationCommandOptionType } from "discord.js"
+import { ApplicationCommandOptionType, ButtonStyle, ComponentType } from "discord.js"
 import { CommandBase, CommandBaseCategory } from "../../types/commandBase"
+import { sEmbed } from "../../types/discord/sEmbed"
+import { sButtonActionRow } from "../../types/discord/sMessageActionRow"
+import { sMessageButton } from "../../types/discord/sMessageButton"
+import { gifs_kiss, gifs_slap } from "../../utils/gifs"
 
 export const command: CommandBase = {
     data: {
@@ -8,17 +12,109 @@ export const command: CommandBase = {
         options: [
             {
                 name: "usuario",
-                description: "Quem deseja beijar",
+                description: "A pessoa que você deseja beijar",
                 type: ApplicationCommandOptionType.User,
                 required: true
             }
         ]
     },
-    enabled: false,
+    enabled: true,
     ephemeral: false,
     category: CommandBaseCategory.FUN,
     needsDefer: true,
-    async run(interaction, client) {
-        return
-    },
+
+    async run({interaction, client}) {
+        const embed = new sEmbed
+
+        const user = interaction.options.getUser("usuario")
+        if (!client.user) return
+        if (!user) return
+        if (!interaction.channel) return
+
+        if (user.id == client.user.id) {
+            embed.setDescription(`**Vamos manter nossa relação como uma amizade, ok ${interaction.user}?**`)
+            await interaction.editReply({ embeds: [embed] })
+            return
+        }
+
+        // else if (user == interaction.user) {
+        //     embed.setTitle("O amor está no ar!  :heart:")
+        //         .setDescription(`${interaction.user} beijou ${user}`)
+        //         .setImage(gifs_kiss[client.random(0, gifs_kiss.length)])
+        //         .setFooter({
+        //             text: "Amar a si mesmo é bom!",
+        //         })
+        //     await interaction.editReply({ embeds: [embed] })
+        //     return
+        // }
+
+        const dateNow = Date.now()
+
+        embed.setTitle("O amor está no ar!  :heart:")
+            .setDescription(`${interaction.user} beijou ${user}`)
+            .setImage(gifs_kiss[client.random(0, gifs_kiss.length)])
+            .setFooter({
+                text: `Requisitado por ${interaction.user.username}`,
+                iconURL: interaction.user.displayAvatarURL({
+                    extension: "png",
+                    forceStatic: true
+                })
+            })
+        
+        const repeat = new sMessageButton()
+            .setCustomId(`repeat${dateNow}`)
+            .setEmoji("🔁")
+            .setLabel("Retribuir")
+            .setStyle(ButtonStyle.Primary)
+
+        const reject = new sMessageButton()
+            .setCustomId(`reject${dateNow}`)
+            .setEmoji("❌")
+            .setLabel("Recusar")
+            .setStyle(ButtonStyle.Primary)
+
+        const row = new sButtonActionRow()
+            .addComponents(reject, repeat)
+
+        await interaction.editReply({
+            content: null,
+            embeds: [embed],
+            components: [row]
+        })
+
+        const collector = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.Button,
+            max: 1,
+            time: 180000,
+            filter: (btnInt) => btnInt.user.id == user.id
+        })
+
+        collector.on("collect", async (i) => {
+            if (i.customId === `repeat${dateNow}`) {
+                interaction.editReply({ components: [row] })
+
+                const embedRetribuir = new sEmbed().setTitle("As coisas estão pegando fogo aqui!  :fire:")
+                    .setDescription(`${i.user} retribuiu o beijo de ${interaction.user}\nSerá que temos um novo casal aqui?  :heart:`)
+                    .setImage(gifs_kiss[client.random(0, gifs_kiss.length)])
+
+                await i.reply({ embeds: [embedRetribuir] })
+            }
+            else if (i.customId === `reject${dateNow}`) {
+                interaction.editReply({ components: [row] })
+
+                const embedRetribuir = new sEmbed().setTitle(`Quem nunca levou um fora, né ${interaction.user.username}`)
+                    .setDescription(`${i.user} negou o beijo de ${interaction.user}  :broken_heart:`)
+                    .setImage(gifs_slap[client.random(0, gifs_slap.length)])
+
+                await i.reply({ embeds: [embedRetribuir] })
+            }
+        })
+
+        collector.on("end", async () => {
+            repeat.setDisabled(true)
+            reject.setDisabled(true)
+            repeat.setDisabled(true)
+            reject.setDisabled(true)
+        })
+    }
 }
