c6d1a8e1f9799a8517c8bf59369219576eb943c1
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-11-26 23:31:26 -0300

    deleted previous migrations (not in production)

--- prisma/migrations/20221127022915_first_migration/migration.sql
+++ prisma/migrations/20221127022915_first_migration/migration.sql
@@ -64,6 +64,7 @@ CREATE TABLE "Ticket" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "enabled" BOOLEAN NOT NULL,
     "memberId" TEXT NOT NULL,
 
     CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
