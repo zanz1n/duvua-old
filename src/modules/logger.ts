3cc737db8185f958f1f1e88f197005c2031fcd43
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-01 23:35:08 -0300

    unnecessary stuff

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -11,8 +11,8 @@ export const logger = {
     // info(log: string | any) { debug("bot:info")(`\x1b[44mINFO\x1b[0m` + ` ${log}`) },
     // warn(log: string | any) { debug("bot:warn")(`\x1b[43mWARN\x1b[0m` + ` ${log}`) },
     // error(log: string | any) { debug("bot:error")(`\x1b[41mERROR\x1b[0m` + ` ${log}`) },
-    debug (log: any) { debug("bot:debug") (log) },
-    info  (log: any) { debug("bot:info" ) (log) },
-    warn  (log: any) { debug("bot:warn" ) (log) },
-    error (log: any) { debug("bot:error") (log) }
+    debug:  debug("bot:debug"),
+    info:   debug("bot:info" ),
+    warn:   debug("bot:warn" ),
+    error:  debug("bot:error")
 }
