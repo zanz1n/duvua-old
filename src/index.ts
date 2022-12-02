3f3bdb7da3f8bdd43d27450923ae2d1f3300cc6a
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-02 00:28:29 -0300

    some updates

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { Duvua } from "./Client";
-import { config } from "./config";
+import { Duvua } from "./Client"
+import { config } from "./config"
+import { logger } from "./modules/logger"
 
 const TOKEN = config.token
 
 if (!TOKEN || TOKEN.length < 10) {
-    console.error("NO VALID TOKEN PROVIDED\nGOT VALUE \"" + TOKEN + "\"")
+    logger.error("NO VALID TOKEN PROVIDED\nGOT VALUE \"" + TOKEN + "\"")
     process.exit(1)
 }
 
