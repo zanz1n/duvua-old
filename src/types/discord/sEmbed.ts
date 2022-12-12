99be24f88bbfb708da0e4d52365385cc36e28e01
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-12 15:38:09 -0300

    updated sEmbed class with utils

--- src/types/discord/sEmbed.ts
+++ src/types/discord/sEmbed.ts
@@ -3,6 +3,11 @@ import { APIEmbed } from "discord.js"
 import { config } from "../../config"
 
 export class sEmbed extends EmbedBuilder {
+    static utils = {
+        defaultMessage(content: string) {
+            return new sEmbed({ description: `**${content}**` })
+        }
+    }
     constructor(data?: APIEmbed | undefined) {
         super(data)
         this.setColor(config.embedColor)
