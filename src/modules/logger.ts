b6231e9b93beb652d4757403ddeb5f9bb8516700
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-13 15:58:13 -0300

    logger updated

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -11,7 +11,7 @@ export const logger = {
     // info(log: string | any) { debug("bot:info")(`\x1b[44mINFO\x1b[0m` + ` ${log}`) },
     // warn(log: string | any) { debug("bot:warn")(`\x1b[43mWARN\x1b[0m` + ` ${log}`) },
     // error(log: string | any) { debug("bot:error")(`\x1b[41mERROR\x1b[0m` + ` ${log}`) },
-    debug:  debug("bot:debug"),
+    debug:  debug("botd:debug"),
     info:   debug("bot:info" ),
     warn:   debug("bot:warn" ),
     error:  debug("bot:error")
