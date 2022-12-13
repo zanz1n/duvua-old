d1e3bd525cc21311a66f1801c2e1430f61073d0e
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-13 16:00:56 -0300

    interaction interface updated

--- src/types/discord/sInteraction.ts
+++ src/types/discord/sInteraction.ts
@@ -1,5 +1,3 @@
-import { CacheType, ChatInputCommandInteraction } from "discord.js"
+import { ChatInputCommandInteraction } from "discord.js"
 
-export interface sInteraction extends ChatInputCommandInteraction<CacheType> {
-    defered: true
-}
+export interface sInteraction extends ChatInputCommandInteraction { defered: true }
