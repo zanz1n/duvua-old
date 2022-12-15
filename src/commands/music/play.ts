a3f704cdcf72b78666f00dba320e7ddc262689ba
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-15 04:20:42 -0300

    updated play command (not working yet)

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,5 +1,4 @@
-import { Addable } from "@lavaclient/queue"
-import { ApplicationCommandOptionType, GuildMember } from "discord.js"
+import { ApplicationCommandOptionType, GuildMember, PermissionFlagsBits } from "discord.js"
 import { createMentionByUser as men } from "../../modules/createMentionByUser"
 import { logger } from "../../modules/logger"
 import { CommandBase, CommandBaseCategory } from "../../types/commandBase"
@@ -24,59 +23,102 @@ export const command: CommandBase = {
     },
 
     async run({interaction, client}) {
-        if (!interaction.guild) return
-        if (!interaction.member || !(interaction.member instanceof GuildMember)) return
-        if (!interaction.channel) return
+        logger.debug("run")
+        if (!interaction.member || !(interaction.member instanceof GuildMember))
+            throw new Error("CommandInteraction<CacheType>.member not instanceof GuildMember")
+        if (!interaction.guild) throw new Error("CommandInteraction<CacheType>.guild not instanceof Guild")
 
-        const embed = new sEmbed()
+        const { member } = interaction
+        const song = interaction.options.getString("song", true)
 
+        if (!song.startsWith("https://") && song.length > 150) {
+            const embed = sEmbed.utils.defaultMessage(`Pesquisas com mais de 150 caracteres tendem não retornar resultados válidos, ${men(interaction.user)}`)
+            interaction.editReply({ content: null, embeds: [embed] })
+            return
+        }
+
+        const guildDb = await client.dba.guild.getOrCreateFromGuild(interaction.guild)
+
+        if (guildDb.musicStrictM) {
+            const memberDb = await client.dba.member.getOrCreateFromMember(interaction.member)
+
+            if (!memberDb.playAllowed || !interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
+                const embed = sEmbed.utils
+                    .defaultMessage(`Você não tem permissão para tocar músicas nesse servidor ${men(interaction.user)}`)
+                interaction.editReply({ content: null, embeds: [embed] })
+                return
+            }
+        }
+    
         const vc = interaction.member.voice.channel
 
         if (!vc) {
-            embed.setDescription(`**Você precisa estar conectado a um canal de voz para usar esse comando ${men(interaction.user)}**`)
+            const embed = sEmbed.utils
+                .defaultMessage(`Você precisa estar em um canal de voz para usar esse comand, ${men(interaction.user)}`)
             interaction.editReply({ content: null, embeds: [embed] })
             return
         }
-        const player = client.music.players.get(interaction.guild.id)
-        ?? client.music.createPlayer(interaction.guild.id)
+        const node = client.music.getNode()
 
-        const results = await client.music.rest.loadTracks("https://youtu.be/Vk7WCUYSV4A")
+        if (!node) throw new Error("Lavalink node sort failed")
 
-        let tracks: Addable[] = []
-        let msg = ""
-        if (results.loadType == "LOAD_FAILED" || results.loadType == "NO_MATCHES") {
-            embed.setDescription(`**Algo deu errado tentando carregar a música, ${men(interaction.user)}**`).setFooter({
-                text: "Tentar de novo ou verificar se o vídeo não possúi nenhum tipo de restrição pode ser um boa ideia"
+        let player = node.players.get(interaction.guild.id)
+
+        if (!player) {
+            player = await node.joinChannel({
+                channelId: vc.id,
+                guildId: interaction.guild.id,
+                shardId: 0,
+                deaf: true
             })
+            player.on("update", (d) => {
+                logger.debug(d)
+            })
+        }
+
+        const songQuery = song.startsWith("https://") ? song : `scsearch:${song}`
+
+        const result = await node.rest.resolve(songQuery)
+
+        if (!result || result.loadType == "LOAD_FAILED" || result.loadType == "NO_MATCHES" || !result.tracks[0]) {
+            const embed = sEmbed.utils
+                .defaultMessage(`Não foi possível encontrar nenhum som pesquisando por \`${song}\`, ${men(interaction.user)}`)
             interaction.editReply({ content: null, embeds: [embed] })
             return
         }
-        else if (results.loadType == "PLAYLIST_LOADED") {
-            tracks = results.tracks
-            msg = `Playlist loaded with size ${results.tracks.length}`
-        }
-        else if (results.loadType == "SEARCH_RESULT" || results.loadType == "TRACK_LOADED") {
-            const [track] = results.tracks
-            tracks = [track]
-            msg = `Track \`${track.info.title}\` from ${track.info.author} loaded`
-        }
-        embed.setDescription(msg)
 
-        if (!player.connected) {
-            await player.connect(vc.id)
+        const embed = new sEmbed()
+
+        if (result.loadType == "PLAYLIST_LOADED") {
+            let trackN = 1
+            let trackText = ""
+
+            result.tracks.forEach(track => {
+                player!.playTrack(track)
+                trackText += `${trackN} - [${track.info.title}](${track.info.uri}) **por** ${track.info.author} com **duração:** ${track.info.length}\n`
+                trackN++
+            })
+
+            embed.setDescription(`**Playlsit ${result.playlistInfo.name} carrecada com ${result.tracks.length} músicas**\n\n${trackText}`)
         }
 
-        const started = player.playing || player.paused
+        else if (result.loadType == "SEARCH_RESULT" || result.loadType == "TRACK_LOADED") {
+            const track = result.tracks[0]
 
-        interaction.editReply({
-            content: null,
-            embeds: [embed]
+            node.queue.add()
+
+            player.playTrack(track)
+
+            embed.setDescription(`Track [${track.info.title}](${track.info.uri}) **por** ${track.info.author} com **duração:** ${track.info.length}`)
+        }
+        else throw new Error("Lavalink load type no matches")
+
+        embed.setFooter({
+            text: `Requisitado por ${interaction.user.username}`,
+            iconURL: member.displayAvatarURL({ forceStatic: true })
+                ?? interaction.user.displayAvatarURL({ forceStatic: true })
         })
 
-        await player.queue.add(tracks, { requester: interaction.user.id })
-        if (!started) {
-            logger.debug(player)
-            await player.queue.start()
-        }
+        interaction.editReply({ content: null, embeds: [embed] })
     }
 }
