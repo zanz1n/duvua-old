06358287791da7291861b332502a1b3cf104bf51
Izan Rodrigues <izanrodrigues999@gmail.com>
2022-12-15 04:20:04 -0300

    updated scripts

--- scripts/deletedockerinfo.sh
+++ scripts/deletedockerinfo.sh
@@ -3,10 +3,10 @@ docker-compose down
 docker stop $(docker ps -q) >/dev/null 1>/dev/null 2>/dev/null
 docker rm $(docker ps -aq) >/dev/null 1>/dev/null 2>/dev/null
 
-sudo rm -rf .docker-volumes/postgres
-mkdir .docker-volumes/postgres
-sudo rm -rf .docker-volumes/redis
-mkdir .docker-volumes/redis
+sudo rm -rf .docker-volumes/postgres_volume
+mkdir .docker-volumes/postgres_volume
+sudo rm -rf .docker-volumes/redis_volume
+mkdir .docker-volumes/redis_volume
 
 sudo rm -r node_modules
 sudo chown -R $USER:$USER ./
