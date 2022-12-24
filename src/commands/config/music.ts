caafe41cbab7c6d812de1d6e429f89274955b077
zanz1n <izanrodrigues999@gmail.com>
2022-12-24 01:14:00 -0300

    added more options to music config command

--- src/commands/config/music.ts
+++ src/commands/config/music.ts
@@ -1,5 +1,6 @@
 import { ApplicationCommandOptionType, GuildMember, PermissionFlagsBits } from "discord.js"
 import { CommandBase, CommandBaseCategory } from "../../types/commandBase"
+import { createMentionByUser as men } from "../../modules/createMentionByUser"
 
 export const command: CommandBase = {
     category: CommandBaseCategory.MODUTIL,
@@ -50,6 +51,59 @@ export const command: CommandBase = {
                         ]
                     }
                 ]
+            },
+            {
+                type: ApplicationCommandOptionType.SubcommandGroup,
+                name: "strict",
+                description: "Comandos relacionados ao modo estrito de música",
+                descriptionLocalizations: { "en-US": "Strict music mode related commands" },
+                options: [
+                    {
+                        type: ApplicationCommandOptionType.Subcommand,
+                        name: "toggle",
+                        description: "Habilita ou desabilita o modo estrito de música no servidor",
+                        descriptionLocalizations: { "en-US": "Toggle the strict music mode" },
+                        options: [
+                            {
+                                type: ApplicationCommandOptionType.Boolean,
+                                name: "enabled",
+                                description: "O modo estrito deve ser habilitado?",
+                                descriptionLocalizations: { "en-US": "Should the strict mode be enabled?"},
+                                required: true
+                            }
+                        ]
+                    },
+                    {
+                        type: ApplicationCommandOptionType.Subcommand,
+                        name: "add",
+                        description: "Adiciona um usuário a lista de permitidos para tocar",
+                        descriptionLocalizations: { "en-US": "Adds a user to the list of allowed to play" },
+                        options: [
+                            {
+                                type: ApplicationCommandOptionType.User,
+                                name: "user",
+                                description: "O usuário que deve ser adicionado",
+                                descriptionLocalizations: { "en-US": "The user who should be added"},
+                                required: true
+                            }
+                        ]
+                    },
+                    {
+                        type: ApplicationCommandOptionType.Subcommand,
+                        name: "remove",
+                        description: "Remove um usuário da lista de permitidos para tocar",
+                        descriptionLocalizations: { "en-US": "Removes a user of the list of allowed to play" },
+                        options: [
+                            {
+                                type: ApplicationCommandOptionType.User,
+                                name: "user",
+                                description: "O usuário que deve ser removido",
+                                descriptionLocalizations: { "en-US": "The user who should be removed"},
+                                required: true
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     },
@@ -68,7 +122,7 @@ export const command: CommandBase = {
 
         if (subcommandGroup == "dj") {
             const member = interaction.options.getMember("user")
-            if (!member || !(member instanceof GuildMember)) throw new Error("!member || !(member instanceof GuildMember)")
+            if (!member || !(member instanceof GuildMember)) return
             if (subCommand == "add") {
                 const memberDb = await client.dba.member.updateOrCreateMember(member, { dj: true })
                 client.utils.createDefaultReply(interaction, `<@${memberDb.userId}> é um dj`)
@@ -78,6 +132,27 @@ export const command: CommandBase = {
                 const memberDb = await client.dba.member.updateOrCreateMember(member, { dj: false })
                 client.utils.createDefaultReply(interaction, `<@${memberDb.userId}> não é mais um dj`)
                 return
+            }
+        }
+        else if (subcommandGroup == "strict") {
+            if (subCommand == "toggle") {
+                const enabled = interaction.options.getBoolean("enabled", true)
+                const guildDb = await client.dba.guild.updateOrCreateGuild(interaction.guild, {
+                    musicStrictM: enabled
+                })
+                client.utils.createDefaultReply(interaction,
+                    `O modo estrito de música foi ${guildDb.musicStrictM ? "habilitado" : "desabilitado"}, {USER}`)
+                return
+            }
+            const member = interaction.options.getMember("user")
+            if (!member || !(member instanceof GuildMember)) return
+            if (subCommand == "add") {
+                client.dba.member.updateOrCreateMember(member, { playAllowed: true })
+                client.utils.createDefaultReply(interaction, `${men(member.user)} agora pode tocar músicas no servidor`)
+            }
+            else if (subCommand == "remove") {
+                client.dba.member.updateOrCreateMember(member, { playAllowed: false })
+                client.utils.createDefaultReply(interaction, `${men(member.user)} agora não pode mais tocar músicas no servidor`)
             }
         }
     }
