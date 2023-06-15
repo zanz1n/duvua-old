9e72bea48d7f3d0478ecb6b2914b73b1f32b2f2b
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-06-15 20:42:06 -0300

    adding more features

---
+++ shared/utils/session.go
@@ -0,0 +1,23 @@
+package utils
+
+import "github.com/bwmarrin/discordgo"
+
+func NewSession(token string, intent *discordgo.Intent) (*discordgo.Session, error) {
+	client, err := discordgo.New("Bot "+token)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if intent == nil {
+		client.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
+	} else {
+		client.Identify.Intents = *intent
+	}
+
+	if err = client.Open(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
